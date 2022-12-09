3e235efd8b544ea99fa92942c5dedd249ac1302c
shiviljude22 <shiviljude22@gmail.com>
2022-12-08 18:30:24 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 {Success}
+ */
+
+int main(void)
+{
+	puts("\"Programminging is like building a multilingual puzzle");
+	return (0);
+
+}
