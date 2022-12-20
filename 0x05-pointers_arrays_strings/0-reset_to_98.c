7a7a07b77b3367311394b1413635d68d7ecf2d4d
shiviljude22 <shiviljude22@gmail.com>
2022-12-19 18:51:41 -0800

    reseting the value of pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * reset_to_98 -> a function to update the reference of a pointer
- * @n: pointer to n to be updated
+ * reset_to_98 - take a pointer to an int as parameter and update
+ * the value it point to 98
+ * @n: point to an int
  */
 void reset_to_98 (int *n);
 {
-	x = 98;
-	*n = &x;
+	*n = 98;
 }
