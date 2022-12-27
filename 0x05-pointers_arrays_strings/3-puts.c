232d3002c874fcb81daa6869fd57233b71d18561
shiviljude22 <shiviljude22@gmail.com>
2022-12-26 22:05:37 -0800

    a function that prints a string, in reverse, followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: string to be printed
+ * Return: return always nothing (success)
+ */
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(char);
+		str++;
+	}
+	_putchar('\n');
+}
