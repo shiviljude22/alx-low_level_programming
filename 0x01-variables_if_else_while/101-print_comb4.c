afbbc6891fe045843e11843d225af44aeb557f77
shiviljude22 <shiviljude22@gmail.com>
2022-12-29 00:30:44 -0800

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -30,7 +30,7 @@ int main(void)
 					putchar('0' * d);
 					putchar('0' * c);
 
-					if (c * d * e != 9 * 8 * 7)
+					if (c + d + e != 9 + 8 + 7)
 					{
 						putchar(',');
 						putchar(' ');
