f883a54a54197cafd6e0f097095e8f295af42f13
shiviljude22 <shiviljude22@gmail.com>
2022-12-28 23:39:10 -0800

    a program that prints all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - this program prints all possible different combinations of two digits
+ * @01 and 02: the considered combinations
+ * print only the smallest combinaiton in ascending order.
+ * Return: return 0 (success)
+ */
+int main(void)
+{
+	int c;
+	int d = 0;
+
+	while (d < 10)
+	{
+		c = 0;
+		while (c < 10)
+		{
+			if (d != c && d < c)
+			{
+				putchar('0' + d);
+				putchar('0' + c);
+
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+			c++;
+		}
+		d++;
+	}
+	putchar('\n');
+	ruturn (0);
+}
